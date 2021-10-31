6fd0c0a47d2775f547b7bea94f1829d19120d33e
MANAN743 <malhotrasamta@gamil.com>
2021-10-31 17:26:15 +0530

    spectagram

---
+++ config.js
@@ -0,0 +1,9 @@
+ export const firebaseConfig = {
+    apiKey: "AIzaSyAtKdXg3vbZ-6M5-SGu93Nl_ym8lQ1yXq0",
+    authDomain: "spectagram-1fc7b.firebaseapp.com",
+    databaseURL: "https://spectagram-1fc7b-default-rtdb.firebaseio.com",
+    projectId: "spectagram-1fc7b",
+    storageBucket: "spectagram-1fc7b.appspot.com",
+    messagingSenderId: "850966010507",
+    appId: "1:850966010507:web:b5a18bb92b796830e1128e"
+  };
